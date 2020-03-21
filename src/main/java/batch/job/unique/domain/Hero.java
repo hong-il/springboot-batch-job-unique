71fe433d2e1b474b32d6fa607d1a3243e924fb36
KIM HONGIL <clroyan01@gmail.com>
2020-03-21 21:43:34 +0900

    Create domain - hero

---
+++ src/main/java/batch/job/unique/domain/Hero.java
@@ -0,0 +1,33 @@
+package batch.job.unique.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@EntityListeners(AuditingEntityListener.class)
+@Entity
+public class Hero {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int age;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @Builder
+    public Hero(Long id, String name, int age, LocalDateTime createdDate) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.createdDate = createdDate;
+    }
+}
