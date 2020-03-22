8eeca05b6515591779641a7b925167bfcf8d3592
KIM HONGIL <clroyan01@gmail.com>
2020-03-22 21:43:34 +0900

    Create batch project

---
+++ src/test/java/batch/job/unique/UniqueApplicationTests.java
@@ -0,0 +1,13 @@
+package batch.job.unique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniqueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
